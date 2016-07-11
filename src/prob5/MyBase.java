e84603e067559f3cc4baf74b419c735345f8668e
donghyo <eotkdehwnd12@naver.com>
2016-07-11 14:05:22 +0900

    2016.07.11

--- src/prob5/MyBase.java
+++ src/prob5/MyBase.java
@@ -9,9 +9,13 @@ public class MyBase extends Base{
 	@Override
 	public void service(String state) {
 		if(state.equals("오후")) {
-			System.out.println("오후도 낮과 마찬가지로 일해야 합니다.");
+			afternoon();
 			return;
 		}
 		super.service(state);
+	}
+	
+	public void afternoon() {
+		System.out.println("오후도 낮과 마찬가지로 일해야 합니다.");
 	}
 }
